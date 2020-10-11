3a87b77cc3614fad394a10609f00a5ebc65975ff
= <cl18083620407@163.com>
2020-10-11 23:17:01 +0800

    Initial commit

---
+++ mybase.py
@@ -0,0 +1,29 @@
+
+# 安装PySide2 : pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyside2
+from PySide2.QtWidgets import QApplication, QMessageBox
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtCore import QFile
+
+class MyBase:
+    def __init__(self):
+        # 从文件中加载UI定义
+        qfile_base = QFile('ui/mybase.ui')
+        qfile_base.open(QFile.ReadOnly)
+        qfile_base.close()
+
+        # 从UI定义中动态创建一个相应的窗口对象
+        # 注意：里面的控件对象也成为窗口对象的属性了
+        # 比如self.ui.button, self.ui.textEdit
+        self.ui = QUiLoader().load(qfile_base)
+        self.ui.searchBtn1.clicked.connect(self.searchBtnClick)
+
+    def searchBtnClick(self):
+        print('search Btn Click...')
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    mybase1 = MyBase()
+    mybase1.ui.show()
+    mybase1.searchBtnClick()
+    app.exec_()
---
+++ word_txt.py
@@ -0,0 +1,50 @@
+
+from docx import Document
+from docx.shared import Inches
+
+# pip3 install python-docx
+# 创建word文档对象
+document1 = Document()
+document = Document("book/docxs/择天记.docx")
+# 添加标题
+document1.add_heading('择天记', 0)
+
+# 前一行
+lastLine = ''
+count = 0
+nextLine = ''
+readNextLine = False
+
+all_paragraphs = document.paragraphs
+for paragraph in all_paragraphs:
+    line = paragraph.text
+    if line in '\n' \
+            or '.com' in line \
+            or 'http:' in line \
+            or '----' in line:
+        continue
+
+    if readNextLine == True:
+        nextLine = line
+        readNextLine = False
+        continue
+
+    wrod = "一声响"
+    if wrod in line:
+        linnn = line.split(wrod)
+        newLine = linnn[0] + '__' + wrod + '__' + linnn[1]
+        print('下一行：', nextLine)
+        print('-------')
+        print('上一行：', lastLine)
+        print('拆分后：', newLine)
+        # 记录下一行
+        readNextLine = True
+        # print('当前行：', paragraph.text)
+        document1.add_paragraph(paragraph.text)
+
+    lastLine = line
+    count += 1
+    if count == 100000:
+        break
+
+document1.save("book/docxs/择天季笔录.docx")
