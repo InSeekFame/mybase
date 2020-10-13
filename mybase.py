c5a351c3802d7206e53acc3d09dcfec3b97ce5bc
inSeekFame <cl18083620407@163.com>
2020-10-13 21:15:52 +0800

    1111

--- mybase.py
+++ mybase.py
@@ -22,6 +22,7 @@ class MyBase:
         self.ui = QUiLoader().load(qfile_base)
         self.ui.searchBtn1.clicked.connect(self.searchBtnClick)
         self.ui.saveWordBtn.clicked.connect(self.saveWordsClick)
+        self.ui.saveTextBtn.clicked.connect(self.exit)
 
         # openAction = QAction()
         # openAction.setShortcut('Ctrl+o')
@@ -30,18 +31,61 @@ class MyBase:
 
     def exit(self):
         print('ctrl+o')
+        f = open("book/txt/搜神记[www.xiashu.cc].txt")  # 返回一个文件对象
+        line = f.readline()  # 调用文件的 readline()方法
+        while line:
+
+            # print(line, end = '')　      # 在 Python 3 中使用
+            line = f.readline()
+            keyWords = self.ui.lineEdit.text()
+            if keyWords in line:
+                print(line)  # 后面跟 ',' 将忽略换行符
+                self.ui.textEdit3.insertPlainText(line+'\n')
+                pass
+        f.close()
+
+    def saveTextClick(self):
+        print('\n\n save text')
+        document = Document('book/docxs/mybase/小说例句集录.docx')
+        text = self.ui.textEdit3.toPlainText() + '\n'
+        document.add_paragraph(text)
+        document.save('book/docxs/mybase/小说例句集录.docx')
 
     def saveWordsClick(self):
         print('------修改后的名词解释------')
         print(self.ui.textEdit1.toPlainText())
-        document = Document("book/docxs/mybase/小说名词集录.docx")
+        temp_Document = Document("book/docxs/mybase/temp.docx")
+        new_key_word = ''
+        has_this_word = False
+        for line in temp_Document.paragraphs:
+            if '【' in line.text and '】' in line.text:
+                print(line.text)
+                new_key_word = line.text
+                break
+        # return
         keyWords = '【' + self.ui.lineEdit.text() + '】'
         isEdit = False
-        # 在最末行添加
+        # # 在最末行添加
         # document.add_paragraph(self.ui.textEdit1.toPlainText())
         # document.save('book/docxs/mybase/小说名词集录.docx')
-
-        print('总行数：',document.paragraphs)
+        document = Document("book/docxs/mybase/小时例句集录.docx")
+        print('总行数：', document.paragraphs)
+        for paragraph in document.paragraphs:
+            if new_key_word in paragraph.text:
+                print('%@==%@', paragraph.text)
+                print('%@==%@', new_key_word)
+                has_this_word = True
+                break
+        if has_this_word:
+            return
+        else:
+            # 添加新的词汇例句
+            print('# 添加新的词汇例句')
+            for paragraph in temp_Document.paragraphs:
+                print(paragraph.text)
+                document.add_paragraph(paragraph.text)
+                document.save('book/docxs/mybase/小时例句集录.docx')
+        return
 
         for index in range(len(document.paragraphs)):
             paragraph = document.paragraphs[index]
@@ -85,7 +129,7 @@ class MyBase:
         # 搜索名词
         self.searchWords()
         # 搜索句子
-        # self.searchParagraphs()
+        self.searchParagraphs()
 
     # 从关键字获取句子
     def searchParagraphs(self):
@@ -94,10 +138,13 @@ class MyBase:
 
         # pip3 install python-docx
         # 创建word文档对象
-        document1 = Document()
+        document1 = Document()  # 临时目录
+        wordText = '【' + self.ui.lineEdit.text() + '】'
+        document1.add_heading(wordText, 3)
+
         document = Document("book/docxs/择天记.docx")
         # 添加标题
-        document1.add_heading('择天记', 0)
+        # document1.add_heading('择天记', 0)
 
         # 前一行
         lastLine = ''
@@ -140,14 +187,21 @@ class MyBase:
                 # 记录下一行
                 readNextLine = True
                 # print('当前行：', paragraph.text)
-                # document1.add_paragraph(paragraph.text)
+                document1.add_paragraph(nextLine+'\n\n')
+                document1.add_paragraph('\n\n')
+                # document1.add_paragraph('\n\n')
+                document1.add_paragraph(lastLine+'\n')
+                document1.add_paragraph(newLine+'\n', style='Intense Quote')
+
 
             lastLine = line
             count += 1
-            if count == 1000000:
+            if count == 10000:
                 break
 
-        # document1.save("book/docxs/择天季笔录.docx")
+        document1.add_paragraph('---end---')
+        document1.add_paragraph('\n\n')
+        document1.save("book/docxs/mybase/temp.docx")
 
 if __name__ == "__main__":
     app = QApplication([])
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # https://blog.csdn.net/zhengxiangwen/article/details/55148287
 
-f = open("foo.txt")  # 返回一个文件对象
+f = open("book/txt/搜神记[www.xiashu.cc].txt")  # 返回一个文件对象
 line = f.readline()  # 调用文件的 readline()方法
 while line:
     print(line),  # 后面跟 ',' 将忽略换行符
@@ -9,7 +9,7 @@ while line:
 
 f.close()
 
-f = open("c:\\1.txt", "r")
-lines = f.readlines()  # 读取全部内容 ，并以列表方式返回
-for line in lines:
-    print(line)
+# f = open("c:\\1.txt", "r")
+# lines = f.readlines()  # 读取全部内容 ，并以列表方式返回
+# for line in lines:
+#     print(line)
